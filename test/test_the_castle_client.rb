8db7ac1bfeb62a16c662df50106ec4af1d116abb
kookster <andrew@beginsinwonder.com>
2011-10-11 12:35:22 -0400

    fix test.

--- test/test_the_castle_client.rb
+++ test/test_the_castle_client.rb
@@ -3,16 +3,11 @@ require 'helper'
 class TestTheCastleClient < Test::Unit::TestCase
 
   def setup 
-    TheCastleClient.key     = 'key'
-    TheCastleClient.secret  = 'secret'
+    TheCastleClient.key     = '9NmQzsWBWs8TyfJswqMmkmjwAH3ItNBIC72KLjQK'
+    TheCastleClient.secret  = '592BSoaCb4FfwqsZ6Ql8WShVZ6HVnecR4Dk9lRfQ'
     TheCastleClient.host    = "development.prx.org"
-    TheCastleClient.port    = 3001
+    TheCastleClient.port    = 3000
     TheCastleClient.version = 'v1'
-  end
-
-  def test_test_fixer
-    response = TheCastleClient.test_fixer
-    puts response.read_body
   end
 
   def test_account_data
@@ -24,12 +19,17 @@ class TestTheCastleClient < Test::Unit::TestCase
     response = TheCastleClient.account_aggregates(7018)
     puts response.read_body
   end
-  
+
   def test_piece_aggregates
     response = TheCastleClient.piece_aggregates(37745)
     puts response.read_body
   end
-  
+
+  def test_piece_data
+    response = TheCastleClient.piece_data(37745, {:scale=>'week', :start_on=>'2010-01-01', :end_on=>'2010-04-01'})
+    puts response.read_body
+  end
+
   def test_popular_pieces
     response = TheCastleClient.popular_pieces(Date.parse('2011-01-03'), 10)
     puts response.read_body
